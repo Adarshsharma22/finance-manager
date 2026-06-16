df611388dd22528dca96d88a52182433a0725029
Adarshsharma22 <adarshsharma6222@gmail.com>
2026-06-16 12:06:30 +0530

    Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .database import engine, SectionLocal
+from .database import engine, SesstionLocal
 from . import models
 from .routers import transactions, categories, reports, budgets, auth
 
